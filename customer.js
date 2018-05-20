3d3be2ca2099eb398977633703a9ea733b572568
David Flores <earlyepitaph@gmail.com>
2018-05-19 19:56:19 -0500

    added node-cli

--- customer.js
+++ customer.js
@@ -1,13 +1,61 @@
 var inquirer = require("inquirer");
 var table = require("cli-table");
 var mysql = require("mysql");
+var inquirer = require("inquirer");
+var Table = require("cli-table");
+
+// Argv vars init
+command = process.argv[2];
 
 
-const table = new Table();
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "",
+  database: "bamazon"
+});
 
-table.push(
-    { 'Some key': 'Some value' }
-  , { 'Another key': 'Another value' }
-);
+connection.connect(function(err) {
+  if (err) throw err;
+  console.clear();
+  console.log("Connected as id: " + connection.threadId);
+  queryAllProductsCLI();
+});
 
-console.log(table.toString());
+
+
+function queryAllProducts() {
+  connection.query("SELECT * FROM products", function(err, res) {
+    console.log("-- ----------------------------- --");
+    console.log("---- -----    Bamazon    ----- ----");
+    console.log("------ --------------------- ------");
+    for (var i = 0; i < res.length; i++) {
+      console.log(res[i].item_id + " | " + res[i].productName+ " | Department: " + res[i].productDepartment + " |  MSRP: " + res[i].productPrice + " | Stock:" + res[i].productStock);
+    }
+
+  });
+  connection.end();
+}
+
+
+
+
+var queryAllProductsCLI = function() {
+  connection.query('SELECT * FROM products', function(err, res) {
+    console.log("-- ----------------------------- --");
+    console.log("---- -----    Bamazon    ----- ----");
+    console.log("------ --------------------- ------");
+      // formatting for npm: cli-table
+          var table = new Table({
+              head: ["ItemID", "ProductName", "Department" ,"MSRP(usd)", "Inventory"],
+              colWidths: [10, 40, 10, 10, 10]
+          });
+      for (var i=0; i < res.length; i++) {
+          var productArray = [res[i].item_id, res[i].productName, res[i].productPrice, res[i].productDepartment, res[i].productStock];
+          table.push(productArray);
+      }
+      console.log(table.toString());
+     // TODO:  productPurchuse();    
+      });
+};
