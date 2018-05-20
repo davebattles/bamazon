fb5ae7c4241fc95da81ea674f185d33574dda537
David Flores <earlyepitaph@gmail.com>
2018-05-19 20:13:04 -0500

    added chalk

--- customer.js
+++ customer.js
@@ -3,6 +3,9 @@ var table = require("cli-table");
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 var Table = require("cli-table");
+var colors = require("colors");
+var ora = require("ora");
+var chalkAnimation = require("chalk-animation");
 
 // Argv vars init
 command = process.argv[2];
@@ -43,19 +46,49 @@ function queryAllProducts() {
 
 var queryAllProductsCLI = function() {
   connection.query('SELECT * FROM products', function(err, res) {
-    console.log("-- ----------------------------- --");
-    console.log("---- -----    Bamazon    ----- ----");
-    console.log("------ --------------------- ------");
+    chalkAnimation.rainbow('Lorem ipsum dolor sit amet');
       // formatting for npm: cli-table
+          
           var table = new Table({
               head: ["ItemID", "ProductName", "Department" ,"MSRP(usd)", "Inventory"],
               colWidths: [10, 40, 10, 10, 10]
           });
       for (var i=0; i < res.length; i++) {
-          var productArray = [res[i].item_id, res[i].productName, res[i].productPrice, res[i].productDepartment, res[i].productStock];
+          var productArray = [res[i].item_id, res[i].productName, res[i].productDepartment, res[i].productPrice, res[i].productStock];
           table.push(productArray);
       }
       console.log(table.toString());
-     // TODO:  productPurchuse();    
+     // TODO:  productPurchase();    
       });
 };
+
+
+function productPurchase () {
+  inquirer
+  .prompt([
+    // Here we create a basic text prompt.
+    {
+      type: "list",
+      message: "What is the ItemID of the Product you want to purchase",
+      choices: ["item","jobs","service"],
+      name: "typeOfProduct"
+    },
+    {
+      type: "input",
+      message: "What is the name of the product?",
+      name: "nameOfProduct"
+    },
+    {
+      type: "input",
+      message: "What is your starting bid? (default 1)",
+      name: "currentBidOfProduct"
+    }
+
+  ])
+  .then(function(inquirerResponse){
+    typeOfProductIn = inquirerResponse.typeOfProduct;
+    nameOfProductIn = inquirerResponse.nameOfProduct;
+    currentBidOfProductIn = inquirerResponse.currentBidOfProduct;
+    addProductInquirer(typeOfProductIn, nameOfProductIn, currentBidOfProductIn);
+  }); 
+}
